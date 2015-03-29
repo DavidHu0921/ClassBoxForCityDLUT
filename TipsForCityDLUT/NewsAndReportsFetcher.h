7b7794d2e6b3851e707bf865d5c3a99bf7d0fa02
DavidHu0921 <IOS&Mac OS Developer>
2015-03-30 00:48:13 +0800

    File added

---
+++ TipsForCityDLUT/NewsAndReportsFetcher.h
@@ -0,0 +1,13 @@
+//
+//  NewsAndReportsFetcher.h
+//  TipsForCityDLUT
+//
+//  Created by 胡啸晨 on 15/3/30.
+//  Copyright (c) 2015年 com.DavidHu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsAndReportsFetcher : NSObject
+
+@end
---
+++ TipsForCityDLUT/NewsAndReportsFetcher.m
@@ -0,0 +1,13 @@
+//
+//  NewsAndReportsFetcher.m
+//  TipsForCityDLUT
+//
+//  Created by 胡啸晨 on 15/3/30.
+//  Copyright (c) 2015年 com.DavidHu. All rights reserved.
+//
+
+#import "NewsAndReportsFetcher.h"
+
+@implementation NewsAndReportsFetcher
+
+@end
