3596d257ada4e046ddaf9a23bdbb90f5a6c4b69f
Eli Lien <elilien@me.com>
2015-04-19 00:44:05 +0800

    Add MR

--- ClassBoxForCityDLUT/AppDelegate.m
+++ ClassBoxForCityDLUT/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <MagicalRecord/CoreData+MagicalRecord.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [self configureMagicalRecord];
+    
     return YES;
 }
 
@@ -41,11 +45,11 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     // Saves changes in the application's managed object context before the application terminates.
-    [self configureMagicalRecord];
+    [MagicalRecord cleanUp];
 }
 
 - (void)configureMagicalRecord {
-    
+    [MagicalRecord setupCoreDataStackWithStoreNamed:@"ClassBoxForCityDLUT.sqlite"];
 }
 
 //#pragma mark - Core Data stack
