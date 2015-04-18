07612ab0159157738f08a877b9f2de86f2f42827
Eli Lien <elilien@me.com>
2015-04-19 00:29:05 +0800

    Update Podfile

---
+++ ClassBoxForCityDLUT/User.m
@@ -0,0 +1,17 @@
+//
+//  User.m
+//  ClassBoxForCityDLUT
+//
+//  Created by Eli Lien on 4/19/15.
+//  Copyright (c) 2015 com.DavidHu. All rights reserved.
+//
+
+#import "User.h"
+
+
+@implementation User
+
+@dynamic username;
+@dynamic passwd;
+
+@end
