daf21ea9df7db7109687bb03f28d9ad2dbf266e8
DavidHu0921 <davidhu920921@gmail.com>
2015-05-02 15:39:42 +0800

    new reuse view

---
+++ ClassBoxForCityDLUT/ReuseView.h
@@ -0,0 +1,13 @@
+//
+//  ReuseView.h
+//  ClassBoxForCityDLUT
+//
+//  Created by 胡啸晨 on 15/5/2.
+//  Copyright (c) 2015年 com.DavidHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReuseView : UICollectionReusableView
+
+@end
---
+++ ClassBoxForCityDLUT/ReuseView.m
@@ -0,0 +1,13 @@
+//
+//  ReuseView.m
+//  ClassBoxForCityDLUT
+//
+//  Created by 胡啸晨 on 15/5/2.
+//  Copyright (c) 2015年 com.DavidHu. All rights reserved.
+//
+
+#import "ReuseView.h"
+
+@implementation ReuseView
+
+@end
