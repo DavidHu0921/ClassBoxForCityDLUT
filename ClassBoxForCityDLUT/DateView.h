c2e6a64397e0254ad8bb2e5f0e8f9e81778b8a3b
DavidHu0921 <davidhu920921@gmail.com>
2015-05-06 19:13:19 +0800

    week view work

---
+++ ClassBoxForCityDLUT/DateView.h
@@ -0,0 +1,13 @@
+//
+//  DateView.h
+//  ClassBoxForCityDLUT
+//
+//  Created by 胡啸晨 on 15/5/6.
+//  Copyright (c) 2015年 com.DavidHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DateView : UIView
+
+@end
---
+++ ClassBoxForCityDLUT/DateView.m
@@ -0,0 +1,21 @@
+//
+//  DateView.m
+//  ClassBoxForCityDLUT
+//
+//  Created by 胡啸晨 on 15/5/6.
+//  Copyright (c) 2015年 com.DavidHu. All rights reserved.
+//
+
+#import "DateView.h"
+
+@implementation DateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
