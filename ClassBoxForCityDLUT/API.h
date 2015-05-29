86e9234dee57616e43621115070d608964db39a2
DavidHu0921 <davidhu920921@gmail.com>
2015-05-30 00:32:55 +0800

    library part is done

--- ClassBoxForCityDLUT/API.h
+++ ClassBoxForCityDLUT/API.h
@@ -8,49 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
-//#define NewsAPI @"https://www.kimonolabs.com/api/bg3nrkz0?apikey=600efbca1fa20767b1ab92a9283f2b1d"
 
-//#define ReportsAPI @"https://www.kimonolabs.com/api/arrsbbjc?apikey=600efbca1fa20767b1ab92a9283f2b1d"
-
-#define NewsAPI @"http://1.csxyxzs.sinaapp.com/xyxw.php"
-#define ReportsAPI @"http://1.csxyxzs.sinaapp.com/xygg.php"
-#define VerifyAPI @"http://1.csxyxzs1.sinaapp.com/verify?name=%@&password=%@"                               //验证登录
-#define ProfileAPI @"http://1.csxyxzs1.sinaapp.com/info?name=%@&password=%@"                                //个人信息
-#define ClassesAPI @"http://1.csxyxzs1.sinaapp.com/schedule?name=%@&password=%@&term=%ld"
-
-//http://1.csxyxzs.sinaapp.com/xyxw.php 20个
-//http://1.csxyxzs.sinaapp.com/xygg.php 20个，时间顺序从新到旧
+#define NewsAPI     @"http://1.csxyxzs.sinaapp.com/xyxw.php"
+#define ReportsAPI  @"http://1.csxyxzs.sinaapp.com/xygg.php"
+#define VerifyAPI   @"http://1.csxyxzs1.sinaapp.com/verify?name=%@&password=%@"                               //验证登录
+#define ProfileAPI  @"http://1.csxyxzs1.sinaapp.com/info?name=%@&password=%@"                                //个人信息
+#define ClassesAPI  @"http://1.csxyxzs1.sinaapp.com/schedule?name=%@&password=%@&term=%ld"
+#define LibraryAPI  @"http://1.csxyxzs.sinaapp.com/library.php?book_name=%@"
 
 /*
- 图书馆 http://1.csxyxzs.sinaapp.com/library.php?book_name=%s
- http://csxyxzs.sinaapp.com/new_query.php?q=[]&p=[]&type=[]
- 
-
  成绩 http://1.csxyxzs1.sinaapp.com/grades?name=%s&password=%s[&term=%s]
  
- halfcrazy 4月28日
- 成绩，课表查询提速。
- 
- halfcrazy 5月7日
  CET 4/6 http://1.csxyxzs1.sinaapp.com/cet?num=%s&name=%s
  
- halfcrazy 5月7日
  快递 http://1.csxyxzs1.sinaapp.com/kuaidi?postid=%snote:返回跳转链接。未处理跳转链接数据
  
- halfcrazy 5月18日
  大连天气：http://v.juhe.cn/weather/index?cityname=%E5%A4%A7%E8%BF%9E&key=9a8dc37abaf5e2e97ccdb17f27765a2a
  
- halfcrazy 5月19日
- 在线代码格式化（可以贴json）http://tool.oschina.net/codeformat/json
- 
- halfcrazy 8月12日
  教师课程表
  http://1.csxyxzs1.sinaapp.com/teacher_schedule?id=%d
  
- halfcrazy 9月5日
  通识课接口
  http://1.csxyxzs1.sinaapp.com/general_course?name=%s&password=%s
  
- halfcrazy 9月7日
  简易版个人信息接口http://1.csxyxzs1.sinaapp.com/info?name=%s&password=%s
  */
--- ClassBoxForCityDLUT/LibraryViewController.m
+++ ClassBoxForCityDLUT/LibraryViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "LibraryViewController.h"
+#import "API.h"
 
-@interface LibraryViewController ()
+@interface LibraryViewController () <UISearchBarDelegate, UIWebViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UISearchBar *librarySearchBar;
+@property (weak, nonatomic) IBOutlet UIWebView *libraryWebView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @end
 
@@ -16,23 +21,77 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
     self.navigationController.navigationBarHidden = NO;
+    
+    self.librarySearchBar.keyboardType = UIKeyboardTypeDefault; // 设置弹出键盘的类型
+//    self.librarySearchBar.tintColor = [UIColor blueColor]; // 设置UISearchBar的颜色 使用clearColor就是去掉背景
+    self.librarySearchBar.placeholder = @"请输入书名"; // 设置提示文字
+    self.librarySearchBar.delegate = self; // 设置代理
+    self.librarySearchBar.showsCancelButton = YES;
+    
+    self.libraryWebView.delegate = self;
+    
+    [self.spinner stopAnimating];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - 实现取消按钮的方法
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"您点击了取消按钮");
+    [searchBar resignFirstResponder]; // 丢弃第一使用者
 }
-*/
+
+#pragma mark - 实现键盘上Search按钮的方法
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"您点击了键盘上的Search按钮");
+    [self.spinner startAnimating];
+    [searchBar resignFirstResponder];
+    
+    [self fetchResult];
+}
+
+- (void)fetchResult{
+    NSString *bookName = self.librarySearchBar.text;
+    NSLog(@"get book : %@", bookName);
+    NSString *bookapi = [NSString stringWithFormat:LibraryAPI, bookName];
+    
+    NSURL *url = [NSURL URLWithString:bookapi];
+    NSURLRequest *request=[NSURLRequest requestWithURL:url];
+    [_libraryWebView loadRequest:request];
+
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    //Check here if still webview is loding the content
+    if (webView.isLoading){
+        return;
+    }
+    else{
+        [self.spinner stopAnimating];
+    }
+}
+
+- (void) setLibraryWebView:(UIWebView *)libraryWebView{
+    _libraryWebView = libraryWebView;
+}
+
+//#pragma mark - 实现监听开始输入的方法
+//- (BOOL)searchBarShouldBeginEditing:(UISearchBar *)searchBar {
+//    NSLog(@"开始输入搜索内容");
+//    
+//    return YES;
+//}
+
+//#pragma mark - 实现监听输入完毕的方法
+//- (BOOL)searchBarShouldEndEditing:(UISearchBar *)searchBar {
+//    NSLog(@"输入完毕");
+//    return YES;
+//}
+
 
 @end
