baa305ab11949d696e37b8a78d086743273163b0
DavidHu0921 <davidhu920921@gmail.com>
2015-05-31 12:27:00 +0800

    some fix

--- ClassBoxForCityDLUT/DiningHallViewController.h
+++ ClassBoxForCityDLUT/DiningHallViewController.h
@@ -2,12 +2,12 @@
 //  DiningHallViewController.h
 //  ClassBoxForCityDLUT
 //
-//  Created by 胡啸晨 on 15/5/29.
+//  Created by 胡啸晨 on 15/5/30.
 //  Copyright (c) 2015年 com.DavidHu. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface DiningHallViewController : UIViewController
+@interface DiningHallViewController : UITableViewController
 
 @end
--- ClassBoxForCityDLUT/DiningHallViewController.m
+++ ClassBoxForCityDLUT/DiningHallViewController.m
@@ -2,7 +2,7 @@
 //  DiningHallViewController.m
 //  ClassBoxForCityDLUT
 //
-//  Created by 胡啸晨 on 15/5/29.
+//  Created by 胡啸晨 on 15/5/30.
 //  Copyright (c) 2015年 com.DavidHu. All rights reserved.
 //
 
@@ -16,15 +16,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
     self.navigationController.navigationBarHidden = NO;
-
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+//    return 3;
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+//    if (section == 0) {
+//        return 8;
+//    }
+//    else if (section == 1){
+//        return 13;
+//    }
+//    else{
+//        return 12;
+//    }
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
 
 /*
 #pragma mark - Navigation
